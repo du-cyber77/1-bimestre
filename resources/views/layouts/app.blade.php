17684a6b0c589b871c17180b81d1abd64457e078
du-cyber77 <eduardosilvadealmeida2@gmail.com>
2025-09-10 12:35:09 -0300

    de novo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="/">
-            <i class="fa-solid fa-school me-2"></i> CIEP 402
+            <i class="fa-solid fa-school me-2"></i> CIEP 1402
         </a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
